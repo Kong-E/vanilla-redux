b19bd5be08d54cb4e338830f0fad2397ba6b62d5
Kong-E <kng001016@gmail.com>
2023-03-01 20:37:28 +0900

    Refactor

--- src/index.js
+++ src/index.js
@@ -4,20 +4,24 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // state는 바뀌는 코드를 말함
 
 // data를 modify하는 function, 오직 얘만 데이터 수정 가능
 // state initializing
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
-  }
-  // return하는 건 너의 어플리케이션의 data가 됨
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  } // return하는 건 너의 어플리케이션의 data가 됨
 };
 
 // store는 data를 저장하는 곳
@@ -32,11 +36,11 @@ countStore.subscribe(onChange);
 
 // countModifier(currentState = 0, {type: "ADD"})
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
