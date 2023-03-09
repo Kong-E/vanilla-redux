7cd717d2476d1b946d8fccd766816d78e683dd1c
Kong-E <kng001016@dgu.ac.kr>
2023-03-09 12:38:00 +0900

    Detail page

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteToDo } from "../store";
+import { Link } from "react-router-dom";
 
 const Todo = (props) => {
   const dispatch = useDispatch();
   const onClick = () => {
     dispatch(deleteToDo(props.id));
   };
-  console.log(props);
 
   return (
     <>
       <li>
-        {props.text}
+        <Link to={`/${props.id}`}>{props.text}</Link>
         <button onClick={onClick}>DELETE</button>
       </li>
     </>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { deleteToDo } from "../store";
 
-const Detail = () => {};
+function Detail() {
+  const param = useParams(); //<Route path="/:id" element={<Detail />} /> 이거랑 연결되어 param에 id 출력
+  const navigate = useNavigate();
+  const toDos = useSelector((state) => state);
+  console.log(toDos);
+  const { id } = param;
+  const detail = toDos.find((toDo) => toDo.id === parseInt(id));
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    dispatch(deleteToDo(detail.id));
+    navigate("/");
+  };
+
+  return (
+    <>
+      <h1>{detail?.text}</h1>
+      <h5>Created at: {detail?.id}</h5>
+      <button onClick={onClick}>DELETE</button>
+    </>
+  );
+}
 
 export default Detail;
